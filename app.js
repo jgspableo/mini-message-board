76201eaf75e393cd7bedd206bb48f972e42cba69
jgspableo <jgspableo@outlook.com>
2026-01-13 18:56:36 +0800

    Add initial files

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+
+const indexRouter = require("./routes/indexRouter");
+const newRouter = require("./routes/newRouter");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", indexRouter);
+app.use("/new", newRouter);
+
+app.use((err, req, res, next) => {
+  if (err) console.error(err);
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
